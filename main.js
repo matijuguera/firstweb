3d52eee194207b84e5de270f6cec89e2455089b8
matijuguera <mati.juguera@gmail.com>
2018-10-07 23:03:27 -0300

    theme selector added

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
         captions: true,
         pager: false
       });
+
+    var theme = $('#theme');
+    $("#toRed").click(function(){
+        theme.attr("href", "css/red.css");
+    })
+    $("#toGreen").click(function(){
+        theme.attr("href", "css/green.css");
+    })
+    $("#toBlue").click(function(){
+        theme.attr("href", "css/blue.css");
+    })
+
   });
